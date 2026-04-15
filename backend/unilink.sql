643d0e0f80ac7b4e4decd4a08c64c6c54ad971ab
Jiin5589 <jimmie.intzes.5589@student.uu.se>
2026-04-15 13:25:23 +0200

    fixade storlek på data och ett syntaxfel

--- backend/unilink.sql
+++ backend/unilink.sql
@@ -4,15 +4,15 @@ USE unilink;
 
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    email VARCHAR (255) UNIQUE,
-    gender VARCHAR(255)
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR (200) UNIQUE,
+    gender VARCHAR(100)
 );
 
 CREATE TABLE courses (
-    course_id VARCHAR (255) PRIMARY KEY,
-    course_name VARCHAR (255),
+    course_id VARCHAR (50) PRIMARY KEY,
+    course_name VARCHAR (255)
 );
 
 
