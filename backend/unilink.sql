d090b921f2047e9f833cbdd1b8c2c6f72d966088
Jiin5589 <jimmie.intzes.5589@student.uu.se>
2026-04-24 11:51:35 +0200

    Async/await istället för callback

--- backend/unilink.sql
+++ backend/unilink.sql
@@ -1,12 +1,16 @@
 DROP DATABASE IF EXISTS unilink;
+DROP USER IF EXISTS 'frontend'@'localhost';
+CREATE USER 'frontend'@'localhost' IDENTIFIED BY 'evavonbahr123';
 CREATE DATABASE unilink;
+GRANT ALL PRIVILEGES ON unilink.* TO 'frontend'@'localhost';
+FLUSH PRIVILEGES;
 USE unilink;
 
+
+-- Har med allt som kommer från front end. 
 CREATE TABLE users (
     user_id VARCHAR (255) NOT NULL PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    email VARCHAR (200) UNIQUE NOT NULL,
+    username VARCHAR(100) UNIQUE NOT NULL,
     gender VARCHAR(100) NOT NULL,
     bio TEXT
 );
@@ -17,19 +21,20 @@ CREATE TABLE courses (
 );
 
 CREATE TABLE events (
-    event_id VARCHAR(255) NOT NULL PRIMARY KEY,
+    event_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id VARCHAR(255),
-    event_name VARCHAR(255) NOT NULL,
+    event_name VARCHAR(255) UNIQUE NOT NULL,
     event_description TEXT NOT NULL,
     event_date DATE NOT NULL,
     event_image VARCHAR(255) NOT NULL,
     event_location TEXT NOT NULL,
-    max_participants INT(11) NOT NULL,
-    FOREIGN KEY user_id REFERENCES users(user_id)
+    max_participants INT NOT NULL,
+    current_participants INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE event_participants (
-    event_id VARCHAR(255) NOT NULL,
+    event_id INT NOT NULL,
     user_id VARCHAR(255) NOT NULL,
     PRIMARY KEY (event_id, user_id),
     FOREIGN KEY (event_id) REFERENCES events(event_id) ON DELETE CASCADE,
@@ -47,4 +52,5 @@ RETURN CONCAT(first_name, ' ', last_name);
 --  3. SELECT funktionen för att göra en "lookup" på en användare eller ett event
 --  4. Ta bort användare från tabeller, se till att alla referenser (cascade borde lösa) försvinner samtidigt
 --  5. Göra lookup baserat på andra specifikationer som ID, namn, kön, etc...
+-- lägga till lösenord
 
